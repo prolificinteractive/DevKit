7afe73d9c29f800893974c5549d78cb7b673c979
Jonathan Samudio <jonathan2457@gmail.com>
2018-09-13 13:55:46 -0400

    Added session manager to DevKit.

--- DevKit/Pods/Target Support Files/Pods-DevKit/Pods-DevKit-frameworks.sh
+++ DevKit/Pods/Target Support Files/Pods-DevKit/Pods-DevKit-frameworks.sh
@@ -143,11 +143,13 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/KeychainAccess/KeychainAccess.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Marker/Marker.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Moulinette/Moulinette.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PureLayout/PureLayout.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/KeychainAccess/KeychainAccess.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Marker/Marker.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Moulinette/Moulinette.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PureLayout/PureLayout.framework"
