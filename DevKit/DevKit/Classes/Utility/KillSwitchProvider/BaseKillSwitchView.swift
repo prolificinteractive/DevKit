14137764571bb429664877ca9656fc3be7f1e8c0
Jonathan Samudio <jonathan2457@gmail.com>
2018-09-17 14:44:23 -0400

    Added base killswitch view documentation.

--- DevKit/DevKit/Classes/Utility/KillSwitchProvider/BaseKillSwitchView.swift
+++ DevKit/DevKit/Classes/Utility/KillSwitchProvider/BaseKillSwitchView.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
+/// The BaseKillSwitchView used to be subclassed to add functionality to the UIView.
+///
+/// **Subspec: Utility/KillSwitchProvider**
+///
+/// ```
+/// final class KillSwitchView: BaseKillSwitchView {
+///
+///     private let descriptionLabel = UILabel()
+///
+///     override func setUserMessage(_ message: String) {
+///         descriptionLabel.setText(message, using: theme.textStyleTheme.bodyNormal.withAlignment(.center))
+///     }
+///
+/// }
+/// ```
+///
+/// The BaseKillSwitchView is a simple class that combines a view with the setUserMessage functionality. The setUserMessage
+/// class should be overrided in the subclass.
+///
 open class BaseKillSwitchView: UIView {
 
     // MARK: - Public Functions
 
     /// Sets the user message with the given parameter.
     ///
-    /// - Parameter message: Message to display to the user.
+    /// - Parameter message: Custom message to display to the user.
     open func setUserMessage(_ message: String) {
         // Override in sub-class
     }
