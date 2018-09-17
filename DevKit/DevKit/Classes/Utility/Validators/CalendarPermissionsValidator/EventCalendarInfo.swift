faa311f2113ea739b40aecdea609dedabf34bc14
Jonathan Samudio <jonathan2457@gmail.com>
2018-09-17 15:11:03 -0400

    Added event info doc.

--- DevKit/DevKit/Classes/Utility/Validators/CalendarPermissionsValidator/EventCalendarInfo.swift
+++ DevKit/DevKit/Classes/Utility/Validators/CalendarPermissionsValidator/EventCalendarInfo.swift
@@ -8,6 +8,20 @@
 
 import Foundation
 
+/// Contains the event info that is used to add to the calendar.
+///
+/// **Subspec: Utility/CalendarPermissionsValidator**
+///
+/// ```
+/// EventCalendarInfo(title: title,
+///                   notes: description,
+///                   startDate: start,
+///                   endDate: start.addingTimeInterval(BusinessConstants.defaultEventInterval))
+/// ```
+///
+/// The EventCalendarInfo model is used with the CalendarPermissionsValidator to encapsulate all of the data needed to add a
+/// new event.
+///
 public struct EventCalendarInfo {
 
     /// Title of the event.
@@ -22,6 +36,13 @@ public struct EventCalendarInfo {
     /// End date of the event.
     public let endDate: Date
 
+    /// Default initializer of the EventCalendarInfo
+    ///
+    /// - Parameters:
+    ///   - title: Title of the event.
+    ///   - notes: Optional notes to add to the event.
+    ///   - startDate: Start date of the event.
+    ///   - endDate: End date of the event.
     public init(title: String, notes: String?, startDate: Date, endDate: Date) {
         self.title = title
         self.notes = notes
